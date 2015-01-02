ce8063cf6251e9e0f48580d37d494a3a6f655cce
Brad Smith <brad@compasshb.com>
2015-01-02 12:49:21 -0800

    Update capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "compasshb.com"
 set :repo_url, "git@github.com:compasshb/compasshb.com.git"
 set :tmp_dir, "/home/wp_9xx2cb/tmp/"
 set :deploy_to, "/home/wp_9xx2cb/www/#{fetch(:application)}"
-set :linked_files, %w{.env wp/wp-config.php wp/wp-content/plugins/wpseo-video wp/wp-content/themes/Total}
+set :linked_files, %w{.env wp/wp-config.php wp/wp-content/uploads wp/wp-content/plugins/wpseo-video wp/wp-content/themes/Total}
 
 namespace :deploy do
 
