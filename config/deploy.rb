1ff7b4d8efb6e5ce1f431a42897166f600b92391
Brad Smith <brad@compasshb.com>
2015-01-03 14:44:38 -0800

    Run symlink:shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,9 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
+      
+      invoke 'deploy:symlink:shared'
+
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
