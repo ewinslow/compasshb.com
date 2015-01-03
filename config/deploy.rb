62e58bcf0044ea2787498161901a073fc3cd5594
Brad Smith <brad@compasshb.com>
2015-01-03 15:52:54 -0800

    Trailing slash

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,6 @@ set :application, "compasshb.com"
 set :repo_url, "git@github.com:compasshb/compasshb.com.git"
 set :tmp_dir, "/home/wp_9xx2cb/tmp/"
 set :deploy_to, "/home/wp_9xx2cb/www/#{fetch(:application)}"
-#set :linked_files, %w{public/.env public/wp-config.php}
-#set :linked_dirs, %w{public/wp-content/uploads public/wp-content/plugins/wpseo-video public/wp-content/themes/Total public/wp-content/plugins/js_composer}
 
 SSHKit.config.command_map[:composer] = "/usr/local/php53/bin/php /home/wp_9xx2cb/.composer/vendor/bin/composer"
 
@@ -18,7 +16,7 @@ namespace :deploy do
       execute "ln -s #{shared_path}/public/.env #{release_path}/public/.env"
       execute "ln -s #{shared_path}/public/.htaccess #{release_path}/public/.htaccess"
       execute "ln -s #{shared_path}/public/wp-config.php #{release_path}/public/wp-config.php"
-      execute "ln -s #{shared_path}/public/wp-content/uploads #{release_path}/public/wp-content/uploads"
+      execute "ln -s #{shared_path}/public/wp-content/uploads/ #{release_path}/public/wp-content/uploads/"
       execute "ln -s #{shared_path}/public/wp-content/plugins/wpseo-video #{release_path}/public/wp-content/plugins/wpseo-video"
       execute "ln -s #{shared_path}/public/wp-content/plugins/js_composer #{release_path}/public/wp-content/plugins/js_composer"
       execute "ln -s #{shared_path}/public/wp-content/themes/Total #{release_path}/public/wp-content/themes/Total"
