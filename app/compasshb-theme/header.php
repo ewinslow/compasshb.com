a68d655f38bacb1dc22e5fc9766712d171ed0eb5
Brad Smith <brad@compasshb.com>
2015-01-13 10:07:27 -0800

    fix(favicon): apple touch icons

--- app/compasshb-theme/header.php
+++ app/compasshb-theme/header.php
@@ -17,8 +17,23 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="apple-touch-icon-precomposed apple-touch-icon apple-touch-startup-image" href="/wp-content/themes/compasshb-theme/images/apple-touch-icon.jpg">
+	<link rel="shortcut icon" href="/wp-content/themes/compasshb-theme/images/favicon.ico">
+	<meta name="apple-mobile-web-app-capable" content="yes">
 	<?php wp_head(); ?>
 	<?php compass_video_og(); ?>
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-53384235-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	
 </head>
 
 <!-- Begin Body -->
