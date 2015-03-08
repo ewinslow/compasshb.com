0d7eaca2e7e719d0b0f0c13b9fd6337884968ad4
Brad Smith <brad@compasshb.com>
2015-03-07 19:48:14 -0800

    chore(youth): update static page

--- resources/views/pages/youth.blade.php
+++ resources/views/pages/youth.blade.php
@@ -12,6 +12,7 @@
         	<div class="row">
                 <div class="col-xs-10 col-xs-offset-1">
                     <h1 class="tk-seravek-web">The United Student Ministry</h1>
+                    <br/>
                 </div>
             </div>
             <div class="row">
@@ -20,13 +21,10 @@
         			<h3>Reaching teens with the Gospel</h3>
         			<p>Every Thursday Night we gather from 6:30-8:30 to hear preaching from the Bible. We teach God’s word in a way that is understandable and applicable for teenagers to live out in their everyday life.</p>
         			<p>Meets at church: 5082 Argosy Avenue, Huntington Beach, CA</p>
-        			<h3>Teaching teens how to live like Jesus</h3>
-        			<p>Every Thursday Night we have a time of small groups with trained leaders who are ready to disciple every student and teach them how to live out God’s word on a day to day basis.</p>
-        			<p>We want to see more teenagers start living for Jesus Christ! If you know anyone who would be interested in checking us out, contact Shane- shane@compassHB.com, or join us on Thursday night here at the church!</p>
         		</div>
         		<div class="col-md-5">
         			<center>
-        				<img src="https://www.compasshb.com/app/uploads/2015/02/UNITED-LOGO.jpg" width="300"/>
+        				<img src="/app/uploads/2015/02/UNITED-LOGO.jpg" width="250"/><br/>&nbsp;
         			</center>
         		</div>
         	</div>
@@ -37,21 +35,10 @@
 
 
                     <div class="col-md-4 col-md-offset-1">
-                        <img width="554" height="554" src="https://www.compasshb.com/app/uploads/2015/02/awakening.png"
-                             alt="AWAKENING"/><br/>
-                        <h4 style="color: #fff" class="tk-seravek-web">AWAKENING</h4>
+                        <h3>Teaching teens how to live like Jesus</h3>
+                        <p>Every Thursday Night we have a time of small groups with trained leaders who are ready to disciple every student and teach them how to live out God’s word on a day to day basis.</p>
+                        <p>We want to see more teenagers start living for Jesus Christ! If you know anyone who would be interested in checking us out, contact Shane- shane@compassHB.com, or join us on Thursday night here at the church!</p>
 
-                        <p><a class="btn btn-default" href="https://compassbiblechurch.ccbchurch.com/form_response.php?id=874">Register Here</a></p>
-
-                        <p>It's when your eyes open for the first time, and you really begin to start seeing. The Bible says what our nation needs today, is an Awakening. For people’s eyes to be opened and to see who Jesus really is, but for that to happen, you have to wake up. Because Awakening starts with you.....</p>
-                        <p><em>"Awake, O sleeper,<br/>and arise from the dead,<br/>and Christ will shine on you."</em><br/> Ephesians 5:14</p>
-                        <p>Join us this year at A W A K E N I N G as we talk about who Jesus is, and how he tells you to wake up.</p>						<ul>
-							<li>Location: Pali Mountain Retreat and Conference Center</li>
-							<li>Departure: Friday, March 6 at 1 p.m. from 5082 Argosy, HB</li>
-							<li>Return: Sunday, March 8 at 2:30 p.m. to 5082 Argosy, HB</li>
-							<li>What to Bring: Bible, Warm Clothes, Pillow, Sleeping Bag, Toiletries & Towel, Flashlight, Extra $ for snacks, & FRIENDS!!</li>
-							<li>Medical Release Forms</li>
-						</ul>
                     </div>
 
                     <div class="col-md-4 col-md-offset-2">
