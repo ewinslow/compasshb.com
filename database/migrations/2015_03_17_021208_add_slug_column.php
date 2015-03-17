9da396e2261929ce9a965fb4f68f062fa459712e
Brad Smith <brad@compasshb.com>
2015-03-17 11:28:23 -0700

    feature(database): slug column

---
+++ database/migrations/2015_03_17_021208_add_slug_column.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSlugColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+
+        Schema::table('sermons', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+
+        Schema::table('passages', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+
+        Schema::table('fellowships', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->string('slug')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('songs', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+
+        Schema::table('sermons', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+
+        Schema::table('passages', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+
+        Schema::table('fellowships', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -139,6 +139,13 @@ class BlogTableSeeder extends Seeder
             'published_at' => '2015-03-17 00:00:00',
         ]);
 
+        Blog::create([
+            'user_id' => 1,
+            'title' => 'Another blog',
+            'body' => 'This is a test blog.',
+            'published_at' => '2015-03-15 00:00:00',
+        ]);
+
         DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
