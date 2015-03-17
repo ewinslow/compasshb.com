78e22d617b6a5441b35b06ecdbaa4616191359d7
Brad Smith <brad@compasshb.com>
2015-03-17 13:37:18 -0700

    chore(notice): scripture of the day

--- resources/views/passages/index.blade.php
+++ resources/views/passages/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <link rel="canonical" href="http://www.compasshb.com/{{ route('read.show', $passages->first()->slug) }}/" />
 <h1 class="tk-seravek-web">Scripture of the Day</h1>
+<div class="alert alert-info" role="alert">This section is being updated. Some comments may not show up while this page is being migrated. If you left a comment earlier today it is not lost and will show up soon. New comments can be added while this work is going on.</div>
   {!! $postflash !!}
   {!! $passage->body !!}
   {!! $passage->verses !!}
