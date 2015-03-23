ac822f07aeec88258f2324d90b993e39493869a3
ewinslow <evan@elgg.org>
2015-03-23 06:34:31 +0000

    chore(bootstrap): Use class name resolution feature of PHP 5.5
    I find this a little cleaner than using strings.
    Allows us to alias the class names.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,18 +27,18 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    'Illuminate\Contracts\Http\Kernel',
-    'CompassHB\Www\Http\Kernel'
+    Illuminate\Contracts\Http\Kernel::class,
+    CompassHB\Www\Http\Kernel::class
 );
 
 $app->singleton(
-    'Illuminate\Contracts\Console\Kernel',
-    'CompassHB\Www\Console\Kernel'
+    Illuminate\Contracts\Console\Kernel::class,
+    CompassHB\Www\Console\Kernel::class,
 );
 
 $app->singleton(
-    'Illuminate\Contracts\Debug\ExceptionHandler',
-    'CompassHB\Www\Exceptions\Handler'
+    Illuminate\Contracts\Debug\ExceptionHandler::class,
+    CompassHB\Www\Exceptions\Handler::class
 );
 
 /*
