1a8377f641bdd8799b3259054d4ae83325f9aa13
Brad Smith <brad@compasshb.com>
2015-03-26 00:21:23 -0700

    feature(scriptureoftheday): spacial scrolling for modern devices with a gyroscope
    tilt your phone to scroll scripture of the day

--- resources/views/passages/show.blade.php
+++ resources/views/passages/show.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.dashboard')
 
 @section('content')
+	<link rel="stylesheet" href="https://raw.githubusercontent.com/VodkaBears/Interdimensional/master/dist/interdimensional.css">
+	<script src="https://raw.githubusercontent.com/VodkaBears/Interdimensional/master/dist/interdimensional.min.js"></script>
+	<script>Interdimensional.charge();</script>
 
 	<h1 class="tk-seravek-web">Scripture of the Day</h1>
   {!! $postflash !!}
