7301e1cc0c60d31e0fd526035c6f6263063f3aa7
Brad Smith <brad@compasshb.com>
2015-04-01 23:05:41 -0700

    chore(sqlite): support for running sqlite database for local dev

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,22 +3,19 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
-
+class CreateUsersTable extends Migration
+{
     /**
      * Run the migrations.
-     *
-     * @return void
      */
     public function up()
     {
-        Schema::create('users', function(Blueprint $table)
-        {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password', 60);
-            $table->timestamp('last_login');
+            $table->nullableTimestamp('last_login');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -26,12 +23,9 @@ class CreateUsersTable extends Migration {
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
     public function down()
     {
         Schema::drop('users');
     }
-
 }
