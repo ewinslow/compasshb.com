364298c2e05380c4b72284b4c3b2e5dc670c2196
Brad Smith <brad@compasshb.com>
2015-04-06 22:52:47 -0700

    feature(series): create series column migration

---
+++ database/migrations/2015_04_05_140820_add_series_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSeriesColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('sermons', function (Blueprint $table) {
+            $table->integer('series_id')->unsigned()->nullable();
+
+            $table->foreign('series_id')
+            ->references('id')
+            ->on('series')
+            ->onDelete('cascade');
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('sermons', function (Blueprint $table) {
+            $table->dropColumn('series_id');
+        });
+    }
+}
