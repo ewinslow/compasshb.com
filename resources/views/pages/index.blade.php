1b1b4fc1e29c79a13c2efe7de73b95422708b485
Brad Smith <brad@compasshb.com>
2015-04-11 22:10:26 -0700

    fix(homepage): twitter widget embed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -136,9 +136,8 @@
       <div class="fb-like-box" data-href="https://www.facebook.com/CompassHB" data-colorscheme="light" data-show-faces="false" data-header="false" data-stream="true" data-show-border="false"></div>
     </div>
     <div class="col-md-7">
-         <h2 class="tk-seravek-web"><a href="https://www.twitter.com/compasshb">Tweets</a></h2>
-         <a class="twitter-timeline" height="400" data-dnt="true" href="https://twitter.com/BradMSmith/lists/compasshb" data-widget-id="566872417012690945" data-chrome="noheader transparent">Tweets from https://twitter.com/BradMSmith/lists/compasshb</a>
-    </div>
+        <h2 class="tk-seravek-web"><a href="https://www.twitter.com/compasshb">Tweets</a></h2><br/>
+        <a class="twitter-timeline" data-dnt="true" href="https://twitter.com/BradMSmith/lists/compasshb" data-widget-id="566872417012690945" data-chrome="noheader">Tweets from https://twitter.com/BradMSmith/lists/compasshb</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>    </div>
 </div>
 <br/><br/>&nbsp;<br/><br/>
 </div>
