a65055e530c6c5f3294566bb1b82d3819dc543e2
Brad Smith <brad@compasshb.com>
2015-04-12 15:17:30 -0700

    chore(series): archive page

--- resources/views/dashboard/series/show.blade.php
+++ resources/views/dashboard/series/show.blade.php
@@ -3,6 +3,32 @@
 @section('content')
 
 	<h1 class="tk-seravek-web">{{ $series->title }}</h1>
-  {!! $series->body !!}
+
+<div class="panel panel-default">
+  <div class="panel-heading tk-seravek-web">Series</div>
+  <div class="panel-body">
+  	<p>{!! $series->body !!}</p>
+  </div>
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Text</th>
+        <th>Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($sermons as $sermon)
+      <tr>
+        <td>{{ $sermon->sku }}</td>
+        <td><a href="{{ route('sermons.show', $sermon->slug) }}">{{ $sermon->title }}</a></td>
+        <td>{{ $sermon->text }}</td>
+        <td>{{ date_format($sermon->published_at, 'l, F j, Y') }}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+</div>
 
 @endsection
