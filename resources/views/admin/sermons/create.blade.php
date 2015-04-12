f98c65b6902d1e4870a0fc9d3d83d159b7d5d084
Brad Smith <brad@compasshb.com>
2015-04-12 08:51:23 -0700

    feature(sundayschool): new ministry page template

---
+++ resources/views/admin/sermons/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.dashboard.master')
+
+@section('content')
+	<h1>Create New Sermon/Lesson</h1>
+
+	@include('errors.list')
+
+	{!! Form::model($sermon = new \CompassHB\Www\Sermon, ['url' => 'sermons', 'files' => true]) !!}
+		@include('admin.sermons.form', ['submitButtonText' => 'Create Sermon'])
+	{!! Form::close() !!}
+
+@endsection
---
+++ resources/views/admin/sermons/form.blade.php
@@ -0,0 +1,44 @@
+<div class="form-group">
+	{!! Form::label('title', 'Title:') !!}
+	{!! Form::text('title', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('worksheet', 'Worksheet:') !!}
+	{!! Form::file('worksheet', ['class' => 'form-control']) !!}
+	<p><a href="{{ $sermon->worksheet }}">{{ $sermon->worksheet }}</a></p>
+</div>
+
+<div class="form-group">
+	{!! Form::label('body', 'Body:') !!}
+	{!! Form::textarea('body', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('teacher', 'Teacher:') !!}
+	{!! Form::text('teacher', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('text', 'Text:') !!}
+	{!! Form::text('text', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('video', 'Video:') !!}
+	{!! Form::text('video', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('sku', 'SKU:') !!}
+	{!! Form::text('sku', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::label('published_at', 'Publish On:') !!}
+	{!! Form::input('date', 'published_at', $sermon->published_at->format('Y-m-d'), ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group">
+	{!! Form::submit($submitButtonText, ['class' => 'btn btn-primary form-control']) !!}
+</div>
