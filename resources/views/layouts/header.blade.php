2b1b2ed61286f0ac6f2de94dbd53374d6c21001c
Evan Winslow <evan.b.winslow@gmail.com>
2015-04-25 00:04:50 +0000

    perf(scripts): Move scripts to bottom of page

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,19 +8,6 @@
     <title>{{{ $title or 'Compass HB' }}} - Compass Bible Church</title>
 
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
-
-
-    <script src="{{ elixir('js/all.js') }}"></script>
-    <script src="//use.typekit.net/gdu1kdg.js"></script>
-    <script>try{Typekit.load();}catch(e){}</script>
-    <?php // @todo <!-- do_action('compass_video_og'); --> ?>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
 </head>
 
 <body>
