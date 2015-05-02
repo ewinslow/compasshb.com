64bf6b2ad0c1d144eac00106b9680bee1ab2802d
Brad Smith <brad@compasshb.com>
2015-05-02 13:45:44 -0700

    fix(photos): masonary layout

--- resources/views/pages/photos.blade.php
+++ resources/views/pages/photos.blade.php
@@ -2,23 +2,42 @@
 
 @section('content')
 </div>
-
+<style>
+.thumbnail { width: 25%;}
+</style>
 <div class="container">
+
+    <div class="row">
 
     <h1 class="tk-seravek-web">Photos</h1>
     <p>View more photos at <a href="http://photos.compasshb.com/">http://photos.compasshb.com/</a></p>
 
 
-    <div class="row">
+      <div id="js-masonry">
         @foreach ($photos as $photo)
-          <div class="col-xs-6 col-md-3">
-            <a href="{{ $photo[0] }}" class="thumbnail">
+          <div class="thumbnail">
+            <a href="{{ $photo[0] }}">
               <img src="{{ $photo[1] }}" alt="compasshb.com/photos">
             </a>
           </div>
         @endforeach
+      </div>
     </div>
 
     <p>View more photos at <a href="http://photos.compasshb.com/">http://photos.compasshb.com/</a></p>
 </div>
+
+<script src="http://imagesloaded.desandro.com/imagesloaded.pkgd.min.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.0/masonry.pkgd.min.js"></script>
+<script>
+var container = document.querySelector('#js-masonry');
+var msnry;
+// initialize Masonry after all images have loaded
+imagesLoaded( container, function() {
+  msnry = new Masonry( container, {
+    "itemSelector": ".thumbnail"
+  } );
+});
+</script>
+
 @stop
