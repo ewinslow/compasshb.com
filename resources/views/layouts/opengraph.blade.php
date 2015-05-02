5383411ab194993f98141e9966d3901987b98539
Brad Smith <brad@compasshb.com>
2015-05-02 15:14:51 -0700

    fix(facebook): preview image og:image tab
    Close #36

---
+++ resources/views/layouts/opengraph.blade.php
@@ -0,0 +1,7 @@
+    <meta property="og:title" content="{{{ $title or 'Compass HB' }}}" />
+    <meta property="og:site_name" content="Compass Bible Church Huntington Beach"/>
+    <meta property="og:url" content="{{ Request::url() }}" />
+    <meta property="og:image" content="http://compasshb.s3.amazonaws.com/images/compasshb-sharing-preview-1200x630.jpg" />
+	<meta property="fb:app_id" content="386571371526429" />
+	<meta property="og:image:height" content="630" />
+	<meta property="og:image:width" content="1200" />
