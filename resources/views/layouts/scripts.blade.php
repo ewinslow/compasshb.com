910a7e394b06b0584fcac3a3db44ae4c16916a3a
Brad Smith <brad@compasshb.com>
2015-05-05 14:32:09 -0700

    chore(facebook): add appid to fix JS warning

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,7 +3,7 @@
 
 <!-- third-party widgets come next because they are secondary UI features -->
 <div id="fb-root"></div>
-<script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0" id="facebook-jssdk" async defer></script>
+<script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=386571371526429" id="facebook-jssdk" async defer></script>
 
 <!-- Analytics comes last because it is not user-facing -->
 <script src="https://www.google-analytics.com/analytics.js" async defer></script>
