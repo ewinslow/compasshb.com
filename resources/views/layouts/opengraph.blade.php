205293d303e5292728005ce122790eeca42b0770
Brad Smith <brad@compasshb.com>
2015-05-05 14:15:35 -0700

    chore(opengraph): use sermon thumb as og:image

--- resources/views/layouts/opengraph.blade.php
+++ resources/views/layouts/opengraph.blade.php
@@ -2,7 +2,7 @@
     <meta property="og:site_name" content="Compass Bible Church Huntington Beach"/>
     <meta property="og:description" content="We are a new church in Huntington Beach committed to preaching straight through the Bible and spreading the gospel of Jesus! Come and see what God is doing among us!"/>
     <meta property="og:url" content="{{ Request::url() }}" />
-    <meta property="og:image" content="{{ $thumb or 'http://compasshb.s3.amazonaws.com/images/compasshb-sharing-preview-1200x630.jpg' }}" />
+    <meta property="og:image" content="{{ $coverimage or 'http://compasshb.s3.amazonaws.com/images/compasshb-sharing-preview-1200x630.jpg' }}" />
 	<meta property="fb:app_id" content="386571371526429" />
 	<meta property="og:image:height" content="630" />
 	<meta property="og:image:width" content="1200" />
