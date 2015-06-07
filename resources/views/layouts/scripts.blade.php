33155743a11c3cb9305b0c50b3d1a90db86b03bc
Brad Smith <brad@compasshb.com>
2015-06-06 19:21:03 -0700

    fix(feedback): hide on homepage
    WEB-123 #5m

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -5,7 +5,7 @@
 <div id="fb-root"></div>
 <script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0&appId=386571371526429" id="facebook-jssdk" async defer></script>
 
-@if(Request::url() !== 'http://compasshb.local')
+@if(Request::url() !== route('home'))
 <!-- Jira Issue Collector -->
 <script type="text/javascript" src="https://jira.compasshb.com:8443/s/89d9d5918b55a1bc8d0c0ba6a23de98c-T/en_US-s0756c/64020/3/1.4.25/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=15b2aa65"></script>
 @endif
