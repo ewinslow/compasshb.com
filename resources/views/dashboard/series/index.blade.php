6d0ab15ea993db500451baaa5880f2971f1bf422
Brad Smith <brad@compasshb.com>
2015-06-16 14:52:00 -0700

    fix(sermons): format index page
    WEB-40 #done
    
    WEB-105 #done #time 20m

--- resources/views/dashboard/series/index.blade.php
+++ resources/views/dashboard/series/index.blade.php
@@ -4,14 +4,16 @@
 
 	<h1 class="tk-seravek-web">Sermon Series</h1><br/>
 
+  <?php $i = 0; ?>
   @foreach ($series as $item)
-  <div class="col-md-4">
+  <div class="col-md-4" {!! ($i % 3) ? '' : 'style="clear: left"' !!}>
     <a href="{{ route('series.show', $item->slug) }}" style="background-image: url({{ $item->image }}); background-size: cover; width: 200px; height: 125px; display: block; background-position: center center;"></a>
     <a href="{{ route('series.show', $item->slug) }}">
       <h4 class="tk-seravek-web"><a href="{{ route('series.show', $item->slug) }}" >{{ $item->title }}</a></h4></a>
       <p>{!! $item->body !!}</p>
     </a>
   </div>
+  <?php $i++; ?>
   @endforeach
 
 @endsection
--- resources/views/dashboard/sermons/index.blade.php
+++ resources/views/dashboard/sermons/index.blade.php
@@ -4,8 +4,9 @@
 
 	<h1 class="tk-seravek-web">Sermons</h1><br/>
 
+  <?php $i = 0; ?>
   @foreach ($sermons as $sermon)
-  <div class="col-md-4">
+  <div class="col-md-4" {!! ($i % 3) ? '' : 'style="clear: left"' !!}>
     <a href="{{ route('sermons.show', $sermon->slug) }}" style="background-image: url({{ $sermon->image }}); background-size: cover; width: 200px; height: 125px; display: block;"></a>
       <h4 class="tk-seravek-web"><a href="{{ route('sermons.show', $sermon->slug) }}" >{{ $sermon->title }}</a></h4>
       <p>{{ $sermon->text }}<br/>
@@ -13,6 +14,7 @@
       {{ $sermon->teacher }}</p>
     </a>
   </div>
+  <?php $i++; ?>
   @endforeach
 
 @endsection
