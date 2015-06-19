3a5a8f7e457f621d0e53bd3aea9a8b7a94c89dca
Brad Smith <brad@compasshb.com>
2015-06-19 13:58:06 -0700

    feature(sermonaudio): transcode sermon video files into mp3s automatically for use in podcast
    WEB-32 #done #time 3h

---
+++ app/Repositories/Transcoder/ZencoderTranscoderRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CompassHB\Www\Repositories\Transcoder;
+
+class ZencoderTranscoderRepository implements TranscoderRepository
+{
+    private $key;
+    private $zencoder;
+    private $bucket = 's3://compasshb/media/';
+
+    public function __construct()
+    {
+        $this->key = env('ZENCODER_KEY', '');
+        $this->zencoder = new \Services_Zencoder($this->key);
+    }
+
+    public function saveAudio($url, $slug)
+    {
+        try {
+            $encoding_job = $this->zencoder->jobs->create(
+                array(
+                    'input' => $url,
+                    'outputs' => array(
+                        array(
+                            'base_url' => $this->bucket,
+                            'filename' => $slug.'.mp3',
+                            'format' => 'mp3',
+                        ),
+                    ),
+                )
+            );
+        } catch (Services_Zencoder_Exception $e) {
+            $bugsnag->notifyError('Info', 'MP3 ZencoderTranscoderRepository Error:'.$e);
+        }
+
+        return $encoding_job;
+    }
+}
