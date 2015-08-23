bb01fff90754799b5faff2ee4ba6a3261bcde27e
Brad Smith <brad@compasshb.com>
2015-08-23 10:41:34 -0700

    chore(live): iframe

--- resources/views/pages/live.blade.php
+++ resources/views/pages/live.blade.php
@@ -6,10 +6,5 @@
 
     <div class="row" style="margin: 20px;">
 
-	<div id="boxcast-widget-20150823-141439-cyrlxfiuuozgctpj" style="width: 640px">
-
-	<script type="text/javascript"> var s = document.createElement('script'); var a = String.fromCharCode; var h = (('https:' == document.location.protocol) ? 'https:' : 'http:'); s.setAttribute('data-action', 'boxcast-widget-load'); s.setAttribute('data-handle', '20150823-141439-cyrlxfiuuozgctpj'); s.setAttribute('data-options', 'version=3'+a(38)+'title=1'+a(38)+'description=1'); s.src = h + '//assets.boxcast.com/widget.js'; document.getElementsByTagName('head')[0].appendChild(s); </script></div>
-
-    </div>
-
+<div id="boxcast-widget-20150823-141504-dgthtrfapiqgexzt" style="width: 640px"><script type="text/javascript"> var s = document.createElement('script'); var a = String.fromCharCode; var h = (('https:' == document.location.protocol) ? 'https:' : 'http:'); s.setAttribute('data-action', 'boxcast-widget-load'); s.setAttribute('data-handle', '20150823-141504-dgthtrfapiqgexzt'); s.setAttribute('data-options', 'version=3'+a(38)+'title=1'+a(38)+'description=1'); s.src = h + '//assets.boxcast.com/widget.js'; document.getElementsByTagName('head')[0].appendChild(s); </script></div>
 @endsection
