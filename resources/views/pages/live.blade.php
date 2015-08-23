d72903d02d8daff8f862d6e80d04eaa157e00640
Brad Smith <brad@compasshb.com>
2015-08-23 08:39:57 -0700

    chore(live): iframe

--- resources/views/pages/live.blade.php
+++ resources/views/pages/live.blade.php
@@ -5,7 +5,11 @@
     <h1 class="tk-seravek-web">Live</h1>
 
     <div class="row" style="margin: 20px;">
-      <iframe src="https://livestream.com/accounts/14660322/events/4273360/player?width=960&height=540&autoPlay=true&mute=false" width="640" height="360" frameborder="0" scrolling="no"> </iframe>
+
+	<div id="boxcast-widget-20150823-141439-cyrlxfiuuozgctpj" style="width: 640px">
+
+	<script type="text/javascript"> var s = document.createElement('script'); var a = String.fromCharCode; var h = (('https:' == document.location.protocol) ? 'https:' : 'http:'); s.setAttribute('data-action', 'boxcast-widget-load'); s.setAttribute('data-handle', '20150823-141439-cyrlxfiuuozgctpj'); s.setAttribute('data-options', 'version=3'+a(38)+'title=1'+a(38)+'description=1'); s.src = h + '//assets.boxcast.com/widget.js'; document.getElementsByTagName('head')[0].appendChild(s); </script></div>
+
     </div>
 
 @endsection
