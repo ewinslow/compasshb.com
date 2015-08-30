c7be94bd1891b5d4dae5812589b5ec3180928758
Brad Smith <brad@compasshb.com>
2015-08-30 09:52:49 -0700

    feature(admin): clear cache button

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,6 +11,8 @@
 @endif
 
 <h1 class="tk-seravek-web">Admin</h1>
-<p>Admin page for posting and scheduling site content.</p><br/>
+<p>Admin page for posting and scheduling site content.</p>
+<br/><br/>
+<p><strong>Shortcuts:</strong><br/><a href="/api/v1/cleareventcache/{{ env('EVENTBRITE_CALLBACK') }}">Force Eventbrite Sync (Clears all event page caches)</a></p>
 
 @endsection
